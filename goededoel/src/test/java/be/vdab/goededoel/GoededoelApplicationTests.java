09896536f0b811a3939649ade1a0feb429a3d02b
Albydo <par.eggermont@gmail.com>
2024-08-22 15:55:44 +0200

    start jenkins

---
+++ goededoel/src/test/java/be/vdab/goededoel/GoededoelApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.goededoel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoededoelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
