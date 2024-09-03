4ade6338f71fc0c4dd06ddb02175ad61ad746c72
Albydo <par.eggermont@gmail.com>
2024-09-03 11:21:28 +0200

    doelen verschillen van andere datatypes test

--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -6,6 +6,8 @@ import be.vdab.goededoel.GoedeDoel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 class GoedeDoelTest {
 	private static final String NAAM = "CliniClowns";
 	private GoedeDoel doel;
@@ -33,6 +35,11 @@ class GoedeDoelTest {
 	@Test
 	void doelMetVerschillendeNaamIsVerschillend(){
 		assertThat(doel).isNotEqualTo(new GoedeDoel(NAAM + "s"));
+	}
+
+	@Test
+	void doelVerschiltVanAnderDataType(){
+		assertThat(doel).isNotEqualTo(BigDecimal.ZERO);
 	}
 
 	/*@Test
